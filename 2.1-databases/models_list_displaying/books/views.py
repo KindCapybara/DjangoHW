6a18ff7a06b44226476c51f886fdf236d86b42a9
KindCapybara <tehsteel@yandex.ru>
2023-12-02 07:20:33 +0300

    add HW-2.1

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from books.models import Book
+
+def index(request):
+    return redirect('books')
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    list_book = Book.objects.all()
+    context = {'list_book': list_book}
+    return render(request, template, context)
+
+
+def book_date(request, pub_date):
+    template = 'books/books_date.html'
+    books_objects = Book.objects.filter(pub_date=pub_date)
+    books_next = (Book.objects.filter(pub_date__gt=pub_date).order_by('pub_date').first())
+    if books_next:
+        books_next = str(books_next.pub_date)
+    else:
+        books_next = None
+    books_previous = Book.objects.filter(pub_date__lt=pub_date).order_by('pub_date').first()
+    if books_previous:
+        books_previous = str(books_previous.pub_date)
+    else:
+        books_previous = None
+
+    context = {
+        'books': books_objects,
+        'next_book': books_next,
+        'previous_book': books_previous,
+    }
     return render(request, template, context)
