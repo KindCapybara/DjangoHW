d24422a7afabe1cfae5555814b900b3cd6d29df1
KindCapybara <tehsteel@yandex.ru>
2023-12-02 06:05:57 +0300

    add HW-2.1

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import Phone
 
 
 def index(request):
@@ -7,11 +8,21 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort = request.GET.get('sort', 'name')
+    phone_object = Phone.objects.all()
+    if sort == "name":
+        phone_object = phone_object.order_by('name')
+    elif sort == "min_price":
+        phone_object = phone_object.order_by('price')
+    elif sort == "max_price":
+        phone_object = phone_object.order_by('-price')
+
+    context = {'phones': phone_object}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
-    return render(request, template, context)
+    phone = Phone.objects.filter(slug__contains=slug).first()
+    context = {'phone': phone}
+    return render(request, template, context=context)
